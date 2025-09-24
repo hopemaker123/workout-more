3babff0455576ad59a9fd25ab6722b60f71c524c
hopemaker202 <hopemaker202@gmail.com>
2025-09-24 18:42:09 +0000

    Changes before Firebase Studio auto-run

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
-import ScrollToTop from "components/ScrollToTop";
-import ErrorBoundary from "components/ErrorBoundary";
-import NotFound from "pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
+import ErrorBoundary from "./components/ErrorBoundary";
+import NotFound from "<div className="" />pages/NotFound";
 import ProfessionalProfile from './pages/professional-profile';
 import ResourceLibrary from './pages/resource-library';
 import OpportunityExplorer from './pages/opportunity-explorer';
